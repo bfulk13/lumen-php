d5f86ed04a1d549683aacecf1f870a1a6b6cd5af
Barry Fulk <barry@iltech.com>
2019-08-19 15:19:20 -0600

    method to edit nested resource

--- app/Http/Controllers/TeacherCourseController.php
+++ app/Http/Controllers/TeacherCourseController.php
@@ -1,6 +1,9 @@
 <?php namespace App\Http\Controllers;
 
 use App\Teacher;
+use App\Course;
+
+use Illuminate\Http\Request;
 
 class TeacherCourseController extends Controller
 {
@@ -22,13 +25,46 @@ class TeacherCourseController extends Controller
         return __METHOD__;
     }
 
-    public function update()
+    public function update(Request $request, $teacher_id, $course_id)
     {
-        return __METHOD__;
+        $teacher = Teacher::find($teacher_id);
+
+        if($teacher)
+        {
+            $course = Course::find($course_id);
+
+            if($course)
+            {
+                $this->validateRequest($request);
+
+                $course->title = $request->get('title');
+                $course->description = $request->get('description');
+                $course->value = $request->get('value');
+                $course->title = $request->get('title');
+
+                $course->save();
+
+                return $this->createSuccessResponse("Course with id {$course_id} was updated.", 200);
+            }
+            return $this->createErrorResponse("Course with id {$course_id} does not exist.", 404);
+        }
+        return $this->createErrorResponse("Teacher with id {$teacher_id} does not exist.", 404);
     }
 
     public function destroy()
     {
         return __METHOD__;
+    }
+
+    function validateRequest($request)
+    {
+        $rules = 
+        [
+            'title' => 'required',
+            'description' => 'required',
+            'value' => 'required'
+        ];
+
+        $this->validate($request, $rules);
     }
 }
