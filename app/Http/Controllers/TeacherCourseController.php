263ce151569d2cf313572ea64d9de2d55b1f95fb
Barry Fulk <barry@iltech.com>
2019-08-19 15:58:34 -0600

    functions to remove nested resources

--- app/Http/Controllers/TeacherCourseController.php
+++ app/Http/Controllers/TeacherCourseController.php
@@ -46,14 +46,57 @@ class TeacherCourseController extends Controller
 
     }
 
-    public function update()
+    public function update(Request $request, $teacher_id, $course_id)
     {
-        return __METHOD__;
+        $teacher = Teacher::find($teacher_id);
+
+		if($teacher)
+		{
+            $course = Course::find($course_id);
+            
+			if($course)
+			{
+                $this->validateRequest($request);
+                
+				$course->title = $request->get('title');
+				$course->description = $request->get('description');
+				$course->value = $request->get('value');
+                $course->teacher_id = $teacher_id;
+                
+                $course->save();
+                
+				return $this->createSuccessResponse("The course with id {$course_id} was updated.", 200);
+			}
+			return $this->createErrorResponse("Course with the id {$course_id} does not exist.", 404);
+		}
+		return $this->createErrorResponse("Teacher with the id {$teacher_id} does not exist", 404);
     }
 
-    public function destroy()
+    public function destroy($teacher_id, $course_id)
     {
-        return __METHOD__;
+        $teacher = Teacher::find($teacher_id);
+
+		if($teacher)
+		{
+            $course = Course::find($course_id);
+            
+			if($course)
+			{
+                if($teacher->courses()->find($course_id))
+                {
+                    // without params passed to detach, you remove all the students
+                    $course->students()->detach();
+
+                    $course->delete();
+
+                    return $this->createSuccessResponse("The course with id {$course_id} was removed", 200);
+                }
+                
+				return $this->createSuccessResponse("The course with id {$course_id} is not associated with the teacher with id {$teacher_id}.", 409);
+			}
+			return $this->createErrorResponse("Course with the id {$course_id} does not exist.", 404);
+		}
+		return $this->createErrorResponse("Teacher with the id {$teacher_id} does not exist", 404);
     }
 
     function validateRequest($request)
